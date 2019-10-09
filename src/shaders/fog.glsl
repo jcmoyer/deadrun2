639263f1182e8b4a78fc29ef0604360e788b7fd3
J.C. Moyer <jcmoyer32@gmail.com>
2019-10-09 05:14:43 -0400

    Bring fog closer to camera

--- src/shaders/fog.glsl
+++ src/shaders/fog.glsl
@@ -3,7 +3,7 @@ uniform highp float fogDensity;
 
 highp vec4 mix_fog(highp vec4 base_color) {
   highp float dist = gl_FragCoord.z / gl_FragCoord.w;
-  highp float fog  = 1.0 / exp(dist * fogDensity) * 2.0;
+  highp float fog  = 1.0 / exp(dist * fogDensity);
   fog = clamp(fog, 0.0, 1.0);
   return mix(fogColor, base_color, fog);
 }
